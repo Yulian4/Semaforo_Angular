3ee638eb6a4789e97f5c50c008dae605e46c1a2a
Yulian4 <yulsyay19@gmail.com>
2025-08-31 20:11:52 -0500

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,49 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('Intro');
+  encendido = signal<boolean>(true);
+  colorActual = signal<'rojo' | 'amarillo' | 'verde'>('rojo');
+
+  mensajeRojo = signal<string>('El rojo es para frenar');
+  mensajeAmarillo = signal<string>('el amarillo por que va a cambiar');
+  mensajeVerde = signal<string>('El verde es para avanzar');
+  mensajeActual = signal<string>(this.mensajeRojo());
+
+  cambiarColor() {
+    if (!this.encendido()) return;
+
+    const color = this.colorActual();
+    if (color === 'rojo') {
+      this.colorActual.set('amarillo');
+      this.mensajeActual.set(this.mensajeAmarillo());
+    } else if (color === 'amarillo') {
+      this.colorActual.set('verde');
+      this.mensajeActual.set(this.mensajeVerde());
+    } else {
+      this.colorActual.set('rojo');
+      this.mensajeActual.set(this.mensajeRojo());
+    }
+  }
+
+  encender() {
+    this.encendido.set(!this.encendido());
+
+    if (!this.encendido()) {
+      this.mensajeActual.set('El semáforo se apagó.');
+    } else {
+      if (this.colorActual() === 'rojo')
+        this.mensajeActual.set(this.mensajeRojo());
+      if (this.colorActual() === 'amarillo')
+        this.mensajeActual.set(this.mensajeAmarillo());
+      if (this.colorActual() === 'verde')
+        this.mensajeActual.set(this.mensajeVerde());
+    }
+  }
 }
